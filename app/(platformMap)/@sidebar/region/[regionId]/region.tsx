74524c42ba1e2404d6fcf4142f7ead8c064ea07f
cgalvarino <charlton@2creek.com>
2026-03-17 20:31:59 +0000

    cleaner logic

--- app/(platformMap)/@sidebar/region/[regionId]/region.tsx
+++ app/(platformMap)/@sidebar/region/[regionId]/region.tsx
@@ -10,20 +10,26 @@ export function RegionList({ region }: { region: Region }) {
   return <ErddapPlatformList boundingBox={region.bbox} />
 }
 
-export function NextRegion({ region, offset }: { region: Region; offset: Number }) {
+export function NextRegion({ region, offset }: { region: Region; offset: number }) {
   let regionIdx = regionList.findIndex((r) => r.slug === region.slug)
   if (regionIdx < 0) {
     return <div />
   }
 
-  let newRegionIdx = (regionIdx + regionList.length + offset) % regionList.length
+  let nextRegionIdx = (regionIdx + regionList.length + offset) % regionList.length
+  let prevRegionIdx = (regionIdx + regionList.length - 1) % regionList.length
+  // Don't show if we only have 1 region or we're trying to show next and it's the same as prev.
+  if (regionIdx == nextRegionIdx || (offset == 1 && nextRegionIdx == prevRegionIdx)) {
+    return <div />
+  }
+
   return (
     <div>
       <Link
-        href={urlPartReplacer(paths.regions.region, ":id", regionList[newRegionIdx].slug as string)}
-        key={regionList[newRegionIdx].name}
+        href={urlPartReplacer(paths.regions.region, ":id", regionList[nextRegionIdx].slug as string)}
+        key={regionList[nextRegionIdx].name}
       >
-        {regionList[newRegionIdx].name}
+        {regionList[nextRegionIdx].name}
       </Link>
     </div>
   )
